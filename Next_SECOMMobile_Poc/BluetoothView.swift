10769b69a4ee0eb014c00ca04da6ad70e1133258
中出恵美 <me-nakade@stsMacBook-Air.local>
2025-08-06 17:54:40 +0900

    fix files

--- Next_SECOMMobile_Poc/BluetoothView.swift
+++ Next_SECOMMobile_Poc/BluetoothView.swift
@@ -1,19 +1,38 @@
 import SwiftUI
+import CoreBluetooth
+
+class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            // スキャン開始
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("発見: \(peripheral), RSSI: \(RSSI)")
+    }
+}
 
 struct BluetoothView: View {
+    @StateObject private var bluetoothManager = BluetoothManager()
     var body: some View {
-        VStack {
-            Text("【Bluetooth連携】")
-        }
-        
         NavigationStack{
             VStack {
+                Text("【Bluetooth連携】")
+
                 NavigationLink("ホームに戻る", destination: ContentView())
             }
         }
     }
 }
-
 #Preview{
     BluetoothView()
 }
